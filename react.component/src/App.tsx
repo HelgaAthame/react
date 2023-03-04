37509f8d5fd0f0ad6fbf3ab66534ce0d1af40be0
HelgaAthame <athamethedarkest@gmail.com>
2023-03-04 15:37:29 +0300

    feat: add error page

--- react.component/src/App.tsx
+++ react.component/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.scss'
 import SearchBar from './SearchBar.js'
 
 function App() {
--- react.component/src/main.tsx
+++ react.component/src/main.tsx
@@ -1,10 +1,23 @@
+import ErrorPage from './error-page.js';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App'
 import './index.scss'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
