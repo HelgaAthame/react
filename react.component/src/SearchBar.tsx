46d26bbf2e62f884a884bf30ddb747adae27b385
HelgaAthame <athamethedarkest@gmail.com>
2023-03-04 14:41:14 +0300

    feat: create search bar

---
+++ react.component/src/SearchBar.tsx
@@ -0,0 +1,13 @@
+import './searchbar.scss';
+
+function SearchBar() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <div className="seach-wrapper">
+      <input type='text' className="seach-bar"/>
+    </div>
+  )
+}
+
+export default SearchBar
