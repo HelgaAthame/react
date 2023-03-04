1299c1da237feea8286e090c9d017d242d87eb44
HelgaAthame <athamethedarkest@gmail.com>
2023-03-04 16:18:15 +0300

    feat: add card structure

--- react.component/src/App.tsx
+++ react.component/src/App.tsx
@@ -1,3 +1,4 @@
+import cards from './cards.js'
 import './App.scss'
 import SearchBar from './SearchBar.js'
 
@@ -7,7 +8,33 @@ function App() {
   history.replaceState(null, '', "main")
   return (
     <div className="App">
+      <Header />
+      <Main />
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <header className="header">
       <SearchBar />
+    </header>
+  )
+}
+
+function Main() {
+  return (
+    <div className="main">
+      {cards.map( (card, i) =>
+        <section className='card' key={i}>
+          <div className='image'><img src={card.picture} /></div>
+          <div className='name'>{card.name}</div>
+          <div className='author'>{card.author}</div>
+          <div className='genre'>Genre: {card.genre}</div>
+          <div className='category'>Category: {card.category}</div>
+          <div className='likes'>🤍 {card.likes}</div>
+        </section>
+      )}
     </div>
   )
 }
---
+++ react.component/src/cards.tsx
@@ -0,0 +1,115 @@
+type Card = {
+  name: string
+  price: number
+  category: string
+  author: string
+  genre: string
+  year: number
+  likes: number
+  picture: string
+}
+
+const cards: Card[] = [
+  {
+    name: 'The Angel of the West Window',
+    price: 1000,
+    category: 'supernatural fiction',
+    author: 'Gustav Meyrink',
+    genre: 'novel',
+    year: 1927,
+    likes: 3,
+    picture: 'https://m.media-amazon.com/images/I/51ZVRx4DPEL._AC_UF1000,1000_QL80_.jpg'
+  },
+  {
+    name: 'The Angel of the West Window',
+    price: 1000,
+    category: 'supernatural fiction',
+    author: 'Gustav Meyrink',
+    genre: 'novel',
+    year: 1927,
+    likes: 3,
+    picture: 'https://m.media-amazon.com/images/I/51ZVRx4DPEL._AC_UF1000,1000_QL80_.jpg'
+  },
+  {
+    name: 'The Angel of the West Window',
+    price: 1000,
+    category: 'supernatural fiction',
+    author: 'Gustav Meyrink',
+    genre: 'novel',
+    year: 1927,
+    likes: 3,
+    picture: 'https://m.media-amazon.com/images/I/51ZVRx4DPEL._AC_UF1000,1000_QL80_.jpg'
+  },
+  {
+    name: 'The Angel of the West Window',
+    price: 1000,
+    category: 'supernatural fiction',
+    author: 'Gustav Meyrink',
+    genre: 'novel',
+    year: 1927,
+    likes: 3,
+    picture: 'https://m.media-amazon.com/images/I/51ZVRx4DPEL._AC_UF1000,1000_QL80_.jpg'
+  },
+  {
+    name: 'The Angel of the West Window',
+    price: 1000,
+    category: 'supernatural fiction',
+    author: 'Gustav Meyrink',
+    genre: 'novel',
+    year: 1927,
+    likes: 3,
+    picture: 'https://m.media-amazon.com/images/I/51ZVRx4DPEL._AC_UF1000,1000_QL80_.jpg'
+  },
+  {
+    name: 'The Angel of the West Window',
+    price: 1000,
+    category: 'supernatural fiction',
+    author: 'Gustav Meyrink',
+    genre: 'novel',
+    year: 1927,
+    likes: 3,
+    picture: 'https://m.media-amazon.com/images/I/51ZVRx4DPEL._AC_UF1000,1000_QL80_.jpg'
+  },
+  {
+    name: 'The Angel of the West Window',
+    price: 1000,
+    category: 'supernatural fiction',
+    author: 'Gustav Meyrink',
+    genre: 'novel',
+    year: 1927,
+    likes: 3,
+    picture: 'https://m.media-amazon.com/images/I/51ZVRx4DPEL._AC_UF1000,1000_QL80_.jpg'
+  },
+  {
+    name: 'The Angel of the West Window',
+    price: 1000,
+    category: 'supernatural fiction',
+    author: 'Gustav Meyrink',
+    genre: 'novel',
+    year: 1927,
+    likes: 3,
+    picture: 'https://m.media-amazon.com/images/I/51ZVRx4DPEL._AC_UF1000,1000_QL80_.jpg'
+  },
+  {
+    name: 'The Angel of the West Window',
+    price: 1000,
+    category: 'supernatural fiction',
+    author: 'Gustav Meyrink',
+    genre: 'novel',
+    year: 1927,
+    likes: 3,
+    picture: 'https://m.media-amazon.com/images/I/51ZVRx4DPEL._AC_UF1000,1000_QL80_.jpg'
+  },
+  {
+    name: 'The Angel of the West Window',
+    price: 1000,
+    category: 'supernatural fiction',
+    author: 'Gustav Meyrink',
+    genre: 'novel',
+    year: 1927,
+    likes: 3,
+    picture: 'https://m.media-amazon.com/images/I/51ZVRx4DPEL._AC_UF1000,1000_QL80_.jpg'
+  }
+]
+
+export default cards
