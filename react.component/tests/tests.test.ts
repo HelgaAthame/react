91d8dcbbb356fc81e9f586d718d4f5f773d1ce26
HelgaAthame <athamethedarkest@gmail.com>
2023-03-08 11:31:26 +0300

    feat: config test environment

---
+++ react.component/tests/tests.test.ts
@@ -0,0 +1,11 @@
+/*
+ * @vitest-environment
+happy-dom
+ */
+
+import { mount } from "@vue/test-utils";
+import {
+  describe,
+  expect,
+  test,
+} from "vitest";
--- react.component/vite.config.ts
+++ react.component/vite.config.ts
@@ -1,11 +1,14 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   server: {
     port: 5000,
   },
+  test: {
+    environment: "happy-dom",
+  }
 })
