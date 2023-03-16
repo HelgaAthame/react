25ece8b25938d66df2f544d0d454d7c6cdd9e457
HelgaAthame <athamethedarkest@gmail.com>
2023-03-16 09:27:33 +0300

    feat: add form pge state

--- react.component/src/form/Form/Form.tsx
+++ react.component/src/form/Form/Form.tsx
@@ -1,4 +1,4 @@
-import { Component, ReactNode } from 'react';
+import { Component, FormEvent, ReactNode } from 'react';
 import './form.scss';
 
 type FormProps = {
@@ -6,7 +6,16 @@ type FormProps = {
 };
 
 export class Form extends Component<FormProps> {
+  handleSubmit(event: FormEvent) {
+    event.preventDefault();
+  }
+
   render() {
-    return <form className="form">{this.props.children}</form>;
+    return <form className="form" onSubmit={this.handleSubmit.bind(this)}>
+      {this.props.children}
+      <div className="submit-wrapper">
+        <input type="submit" className="submit-input" value="SUBMIT"/>
+      </div>
+    </form>;
   }
 }
--- react.component/src/form/FormPage/FormPage.tsx
+++ react.component/src/form/FormPage/FormPage.tsx
@@ -9,6 +9,26 @@ import { File } from '../File';
 import { Checkbox } from '../Checkbox';
 
 export class FormPage extends Component {
+  state = {
+    cards: [],
+    firstName: undefined,
+    lastName: undefined,
+    birthday: undefined,
+    showMyAge: true,
+    profilePhoto: 'https://avatars.mds.yandex.net/i?id=3a61f30a8dda7b409f22c83055b5800984f9830c-8242815-images-thumbs&n=13',
+    zipCode: undefined,
+    country: undefined,
+    city: undefined,
+    address: undefined,
+    email: undefined,
+    receiveMail: true,
+    phone: undefined,
+    receiveSMS: true,
+    firstCheckbox: true,
+    secondCheckbox: true,
+    thirdCheckbox: true,
+  };
+
   render() {
     return (
       <section className="form-page">
@@ -21,7 +41,7 @@ export class FormPage extends Component {
             <Input id="one" label="First Name" type="text" />
             <Input id="two" label="Last Name" type="text" />
             <Input id="three" label="Birthday" type="date" />
-            <Checkbox id="thidteen" title="Show my age" />
+            <Checkbox id="thirteen" title="Show my age" />
             <File id="twelve"/>
           </Fieldset>
 
@@ -41,6 +61,12 @@ export class FormPage extends Component {
             <Switcher key="ten" title="Receive notifications by mail" id="ten"/>
             <Input id="nine" label="Phone" type="text" />
             <Switcher key="eleven" title="Receive sms" id="eleven"/>
+          </Fieldset>
+
+          <Fieldset title="Checkboxes">
+            <Checkbox title="I like this website" id="fourteen" />
+            <Checkbox title="I enjoy filling out forms" id="fifteen" />
+            <Checkbox title="I like reading good books" id="sixteen" />
           </Fieldset>
         </Form>
       </section>
