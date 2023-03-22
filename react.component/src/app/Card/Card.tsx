09b40418018c85b0fb06b7b23dbaa65040c911c7
HelgaAthame <athamethedarkest@gmail.com>
2023-03-22 15:13:14 +0300

    feat: remake Header using hooks

--- react.component/src/app/Card/Card.tsx
+++ react.component/src/app/Card/Card.tsx
@@ -1,11 +1,7 @@
 import { CardT } from '../cards';
 import './card.scss';
 import { ReactComponent as Heart } from '../../assets/heart.svg';
-import { Component, createRef, ReactPropTypes, RefObject, useRef, useState } from 'react';
-
-interface NewCardT extends CardT {
-  cards: CardT[];
-}
+import { useRef, useState } from 'react';
 
 export const Card = (props: CardT) => {
   const heart = useRef(null);
@@ -40,49 +36,4 @@ export const Card = (props: CardT) => {
       </div>
     </section>
   );
-
 }
-
-/*export class Card extends Component<NewCardT> {
-  heart: RefObject<HTMLDivElement>;
-  state = {
-    likes: this.props.likes,
-  };
-
-  constructor(props: NewCardT) {
-    super(props);
-    this.heart = createRef<HTMLDivElement>();
-  }
-
-  handleClick() {
-    this.setState({ likes: this.state.likes + 1 });
-  }
-
-  render() {
-    return (
-      <section className="card">
-        <div className="image">
-          <img src={this.props.picture} />
-        </div>
-        <div className="name">{this.props.name}</div>
-        <div className="author">{this.props.author}</div>
-        <div className="card-footer">
-          <div className="left">
-            <div className="genre">{this.props.genre}</div>
-            <div className="country">{this.props.country}</div>
-          </div>
-          <div
-            ref={this.heart}
-            placeholder="likes"
-            className="likes"
-            onClick={this.handleClick.bind(this)}
-          >
-            <Heart />
-            &nbsp;
-            {this.state.likes}
-          </div>
-        </div>
-      </section>
-    );
-  }
-}*/
--- react.component/src/app/Header/Header.tsx
+++ react.component/src/app/Header/Header.tsx
@@ -1,25 +1,15 @@
 import './header.scss';
-import { cards, CardT } from '../cards';
-import { Component } from 'react';
 import { Logo } from '../Logo';
 import { Nav } from '../Nav';
 
+export const Header = (props: SearchBarProps) => (
+  <header className="header">
+    <Logo curPage={props.currentPage} />
+      {props.children}
+    <Nav />
+  </header>);
+
 type SearchBarProps = {
-  cards?: CardT[];
   children?: React.ReactNode;
   currentPage: string;
 };
-
-export class Header extends Component<SearchBarProps> {
-  state = { cards: cards };
-
-  render() {
-    return (
-      <header className="header">
-        <Logo curPage={this.props.currentPage} />
-        {this.props.children}
-        <Nav />
-      </header>
-    );
-  }
-}
