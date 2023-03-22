bf2ffd843b4ddbfe6ae6241ad6b3ad5793092b23
HelgaAthame <athamethedarkest@gmail.com>
2023-03-22 15:58:01 +0300

    feat: rewrite Search Bar with hooks

--- react.component/src/app/SearchBar/SearchBar.tsx
+++ react.component/src/app/SearchBar/SearchBar.tsx
@@ -1,12 +1,72 @@
-import { ChangeEvent, Component, createRef, RefObject } from 'react';
+import { ChangeEvent, MutableRefObject, useEffect, useRef } from 'react';
 import './searchbar.scss';
 import { ReactComponent as Lupa } from '../../assets/lupa.svg';
+
+export const SearchBar = (props: SearchBarProps) => {
+
+  const wrapper: MutableRefObject<HTMLDivElement | null> = useRef(null);
+  const input: MutableRefObject<HTMLInputElement | null> = useRef(null);
+
+  useEffect(() => {
+    const newValue = localStorage.getItem('bestbookstore-input-data');
+    if (newValue && input && input.current) {
+      input.current.value = newValue;
+      props.updateData(newValue);
+    }
+    return () => {
+      const neededValue = input?.current?.value;
+    if (neededValue) {
+      localStorage.setItem('bestbookstore-input-data', neededValue.toString());
+    } else {
+      localStorage.setItem('bestbookstore-input-data', '');
+    }
+    }
+  }, []);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    props.updateData(event.target.value);
+  }
+
+  const handleFocus = () => {
+    if (wrapper.current !== null) wrapper.current.style.flexGrow = '1';
+    if (input && input.current) input.current.style.color = '#109966';
+  }
+
+  const handleBlur = () => {
+    if (wrapper.current && input && document.activeElement !== input.current) wrapper.current.style.flexGrow = '0';
+    if (input && input.current) input.current.style.color = '#105544';
+  }
+
+  return(
+    <div className="wrapper">
+      <div
+        ref={wrapper}
+        className="search_wrapper"
+        onMouseOver={handleFocus}
+        onMouseOut={handleBlur}
+      >
+        <div className="lupa">
+          <Lupa />
+        </div>
+        <input
+          ref={input}
+          type="search"
+          className="input"
+          onChange={handleChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          required
+        />
+      </div>
+    </div>
+  );
+}
 
 interface SearchBarProps {
   updateData: (cards: string) => void;
 }
 
-export class SearchBar extends Component<SearchBarProps> {
+/*export class SearchBar extends Component<SearchBarProps> {
   wrapper: RefObject<HTMLDivElement>;
   input: RefObject<HTMLInputElement> | undefined;
 
@@ -74,4 +134,4 @@ export class SearchBar extends Component<SearchBarProps> {
       </div>
     );
   }
-}
+}*/
