e6724c6d17dc35b85cfce27dd34963e785000ac7
HelgaAthame <athamethedarkest@gmail.com>
2023-03-22 15:08:16 +0300

    feat: remake Card to hooks

--- react.component/src/app/Card/Card.tsx
+++ react.component/src/app/Card/Card.tsx
@@ -1,13 +1,49 @@
 import { CardT } from '../cards';
 import './card.scss';
 import { ReactComponent as Heart } from '../../assets/heart.svg';
-import { Component, createRef, RefObject } from 'react';
+import { Component, createRef, ReactPropTypes, RefObject, useRef, useState } from 'react';
 
 interface NewCardT extends CardT {
   cards: CardT[];
 }
 
-export class Card extends Component<NewCardT> {
+export const Card = (props: CardT) => {
+  const heart = useRef(null);
+  const [likes, setLikes] = useState(props.likes);
+
+  const handleClick = () => {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <section className="card">
+      <div className="image">
+        <img src={props.picture} />
+      </div>
+      <div className="name">{props.name}</div>
+      <div className="author">{props.author}</div>
+      <div className="card-footer">
+        <div className="left">
+          <div className="genre">{props.genre}</div>
+          <div className="country">{props.country}</div>
+        </div>
+        <div
+          ref={heart}
+          placeholder="likes"
+          className="likes"
+          onClick={handleClick}
+        >
+          <Heart />
+          &nbsp;
+          {likes}
+        </div>
+      </div>
+    </section>
+  );
+
+}
+
+/*export class Card extends Component<NewCardT> {
   heart: RefObject<HTMLDivElement>;
   state = {
     likes: this.props.likes,
@@ -49,4 +85,4 @@ export class Card extends Component<NewCardT> {
       </section>
     );
   }
-}
+}*/
