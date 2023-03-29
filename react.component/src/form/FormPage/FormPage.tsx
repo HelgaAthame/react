706e2cbf8a71700e3772d0d4d6e2dce15cef11c9
HelgaAthame <athamethedarkest@gmail.com>
2023-03-29 16:11:05 +0300

    feat: put form card into a separate compinent

--- react.component/src/form/FormPage/FormPage.tsx
+++ react.component/src/form/FormPage/FormPage.tsx
@@ -5,8 +5,9 @@ import { countries } from '../countries';
 import { Confirmation } from '../Confirmation';
 import { ReactComponent as Upload } from '../../assets/upload.svg';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { FromCard } from '../FormCard';
 
-type ProfileCard = {
+export type ProfileCard = {
   gender: string | undefined;
   firstName: string | undefined;
   lastName: string | undefined;
@@ -529,37 +530,8 @@ export const FormPage = () => {
 
       <div className="cards-section">
         {cards.map((card, index) => (
-          <div className="form-card-wrapper" key={index}>
-            <div className="form-card" placeholder="card">
-              <div className="profile-image-wrapper">
-                <img src={card['upload']} />
-              </div>
-              <div className="form-card__name">Name: {card.firstName + ' ' + card.lastName}</div>
-              <div className="form-card__age">Age: {card.showMyAge ? card.age : 'hidden'}</div>
-              <div className="form-card__age">Gender: {card.gender}</div>
-              <div className="form-card__address">
-                Address:{' '}
-                {card.zipCode + ', ' + card.country + ', ' + card.city + ', ' + card.address}
-              </div>
-              <div className="form-card__email">
-                E-mail: {card.email}&nbsp;
-                {card.receiveMail ? 'R' : "Don't r"}eceive mails
-              </div>
-              <div className="form-card__phone">
-                Phone: {card.phone}&nbsp;
-                {card.receiveSMS ? 'R' : "Don't r"}eceive sms
-              </div>
-              <div className="form-card__first">
-                I{card.firstCheckbox ? ' ' : " don't "}like this website
-              </div>
-              <div className="form-card__second">
-                I{card.secondCheckbox ? ' ' : " don't "}enjoy filling out forms
-              </div>
-              <div className="form-card__third">
-                I{card.thirdCheckbox ? ' ' : " don't "}like reading good books
-              </div>
-            </div>
-          </div>
+          <FromCard card={card} index={index} />
+
         ))}
       </div>
     </section>
