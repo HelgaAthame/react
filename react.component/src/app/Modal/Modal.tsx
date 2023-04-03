8a21f8aaf11e69c0aa025666e9066444be9e5ed0
HelgaAthame <athamethedarkest@gmail.com>
2023-04-03 14:59:17 +0300

    feat: put modal window into the separate component

---
+++ react.component/src/app/Modal/Modal.tsx
@@ -0,0 +1,37 @@
+import { BookType } from "../types";
+import { MouseEventHandler } from 'react';
+
+interface ModalProps extends BookType {
+  handleModalClick: MouseEventHandler<HTMLDivElement>,
+  handleCloseClick: MouseEventHandler<HTMLDivElement>,
+}
+
+export const Modal = (props: ModalProps) => (
+          <div className="modal" onClick={props.handleModalClick}>
+            <div className="close" onClick={props.handleCloseClick}></div>
+            <div className="modal-wrapper">
+              <div className="modal-content">
+                <div className="name">Name: {props.name === '' ? 'unknown' : props.name}</div>
+                <div className="birth">Birth: {props.birth === '' ? 'unknown' : props.birth}</div>
+                <div className="death">Death: {props.death === '' ? 'unknown' : props.death}</div>
+                <div className="gender">
+                  Gender: {props.gender === '' ? 'unknown' : props.gender}
+                </div>
+                <div className="hair">Hair: {props.hair === '' ? 'unknown' : props.hair}</div>
+                <div className="height">
+                  Height: {props.height === '' ? 'unknown' : props.height}
+                </div>
+                <div className="race">Race: {props.race === '' ? 'unknown' : props.race}</div>
+                <div className="realm">Realm: {props.realm === '' ? 'unknown' : props.realm}</div>
+                <div className="spouse">
+                  Spouse: {props.spouse === '' ? 'unknown' : props.spouse}
+                </div>
+                <div className="wiki-url">
+                  URL:{' '}
+                  <a href={props.wikiUrl}>{props.wikiUrl === '' ? 'unknown' : props.wikiUrl}</a>
+                </div>
+                <div className="id">ID: {props._id === '' ? 'unknown' : props._id}</div>
+              </div>
+            </div>
+          </div>
+);
