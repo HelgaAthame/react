4ad2e5d5d0a412ec33e27329356f930bf50bbd94
HelgaAthame <athamethedarkest@gmail.com>
2023-04-15 13:07:00 +0300

    feat: config cypress

---
+++ react.component/cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress';
+import registerCodeCoverageTasks  from '@cypress/code-coverage/task';
+
+export default defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+  e2e: {
+    setupNodeEvents(on, config) {
+      registerCodeCoverageTasks(on, config)
+      // include any other plugin code...
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config
+    },
+  },
+})
