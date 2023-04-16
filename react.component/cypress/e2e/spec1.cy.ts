a3ed28e15cfa59cda14e6acb37dec1c880899a52
HelgaAthame <athamethedarkest@gmail.com>
2023-04-16 11:31:20 +0300

    feat: write somr e2e tests

--- react.component/cypress/e2e/spec1.cy.ts
+++ react.component/cypress/e2e/spec1.cy.ts
@@ -1,6 +1,16 @@
 describe('My first react App e2e tests', () => {
   it('opens nav links', () => {
     cy.visit('/');
-
+    cy.contains('ABOUT US').click();
+    cy.contains('FORM').click();
+    cy.contains('BEST BOOK STORE').click();
+  });
+  it('modal window works', () => {
+    cy.visit('/');
+    cy.contains('Melkor').click();
+    cy.contains('Beleriand');
+    cy.get('.close').click();
+  });
+  it('search works', () => {
   })
-})
+});
