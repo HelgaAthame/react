c6f014e03d802763328d59a02627595f7ec3ca9e
HelgaAthame <athamethedarkest@gmail.com>
2023-04-19 19:03:13 +0300

    feat: remake Logo component styles with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{tsx,ts,html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'regular-font': ['Regular-font'],
+        'medium-font': ['Medium-font'],
+        'light-font': ['Light-font'],
+      },
+      margin: {
+        '-1r': '-1rem',
+      },
+      keyframes: {
+        button: {
+          '0%': {
+            transform: 'scale(5) rotate(0deg)',
+            backgroundImage: 'conic-gradient($color 80%, white 90%)'
+          },
+          '99%': {
+            transform: 'scale(5) rotate(360deg)',
+            backgroundImage: 'conic-gradient($col2 80%, white 90%)'
+          },
+          '100%': {
+            transform: 'scale(5) rotate(360deg)',
+            backgroundIimage: 'conic-gradient($col2 100%, white 0%)'
+          }
+        },
+      },
+      animation: {
+        button: 'button 750ms forwards ease-in-out',
+      }
+    },
+  },
+  plugins: [],
+}
