794f396c85df59cb7ae6d1608e1727633b68a301
HelgaAthame <athamethedarkest@gmail.com>
2023-04-22 19:04:57 +0300

    feat: remake search to tailwind

--- src/app/Nav/Nav.tsx
+++ src/app/Nav/Nav.tsx
@@ -1,19 +1,18 @@
 import { Link } from 'react-router-dom';
-import './nav.scss';
 import React from 'react';
 
 export const Nav = () => (
-  <div className="nav">
-    <div className="link-wrapper">
-      <div className="l-div">
-        <Link to="/about-us" className="link" data-testid="about-us-link">
+  <div className="nav flex flex-wrap">
+    <div className="link-wrapper hover:before:animate-button rounded mt-4 mx-2 duration-1000 flex flex-wrap overflow-hidden relative before:absolute before:bg-[conic-gradient(var(--tw-gradient-stops))] from-white from-0% to-emerald-900 to-0% before:h-full before:w-full before:z-[-1] duration-1000">
+      <div className="l-div rounded-sm px-4 m-px bg-white flex items-center">
+        <Link to="/about-us" className="link text-emerald-900 duration-1000 text-3xl light-font hover:cursor-pointer hover:text-emerald-600" data-testid="about-us-link">
           ABOUT US
         </Link>
       </div>
     </div>
-    <div className="link-wrapper">
-      <div className="l-div">
-        <Link to="/form" className="link" data-testid="form-link">
+    <div className="link-wrapper hover:before:animate-button rounded mt-4 mx-2 duration-1000 flex flex-wrap overflow-hidden relative before:absolute before:bg-[conic-gradient(var(--tw-gradient-stops))] from-white from-0% to-emerald-900 to-0% before:h-full before:w-full before:z-[-1] duration-1000">
+      <div className="l-div rounded-sm px-4 m-px bg-white flex items-center">
+        <Link to="/form" className="link link text-emerald-900 duration-1000 text-3xl light-font hover:cursor-pointer hover:text-emerald-600" data-testid="form-link">
           FORM
         </Link>
       </div>
--- src/app/SearchBar/SearchBar.tsx
+++ src/app/SearchBar/SearchBar.tsx
@@ -1,5 +1,4 @@
 import { KeyboardEvent, ChangeEvent } from 'react';
-import './searchbar.scss';
 import { ReactComponent as SearchIcon } from '../../assets/searchIcon.svg';
 
 import { useDispatch, useSelector } from 'react-redux';
@@ -25,16 +24,17 @@ export const SearchBar = () => {
   };
 
   return (
-    <div className="wrapper">
-      <div className="search_wrapper">
+    <div className="wrapper flex grow justify-start mt-4 mx-2">
+      <div className="search_wrapper bg-white pl-6 rounded-sm border border-emerald-900 border-solid m-0 w-20 flex flex-nowrap text-3xl items-center duration-1000 grow-0 hover:grow hover:border-emerald-600
+      [&:has(.input:focus)]:grow [&:has(.input:hover)]:grow">
         <div className="searchIcon">
-          <SearchIcon />
+          <SearchIcon className="w-8 h-7"/>
         </div>
         <input
           defaultValue={searchText}
           type="search"
           data-testid="search-input"
-          className="input"
+          className="input light-font text-emerald-900 text-3xl outline-none border-none grow w-full my-4 duration-1000"
           onKeyUp={handleKeyUp}
           onChange={handleChange}
           required
